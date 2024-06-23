b51fab0e3c31aa1f8a31d3f081fa8e4d19a7bc6f
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-06-23 08:19:16 +0100

    add `PopularItems`

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { allAssets } from "../apiRequest/ApiRequest";
 import Items from "./Items";
 import { Albums } from "../assets/asset";
+import { PopularItems } from "../assets/popular";
 
 const GalleryItem = () => {
   const [assets, setAssets] = useState([]);
@@ -23,9 +24,13 @@ const GalleryItem = () => {
   }, []);
   console.log(assets);
   return (
-    <div className="flex flex-wrap justify-center gap-5 p-5 overflow-auto h-full">
-      {error && <p>{error}</p>}
+    // <div className="flex flex-wrap justify-center gap-5 p-5 overflow-auto h-full">
+    <div className="h-full overflow-scroll">
+      <div className="bg-red-200 p-5">
+        <h2 className="text-center bg-red-400">MOST POPULAR IN YOUR AREA</h2>
+        {error && <p>{error}</p>}
 
+        <ul className="flex flex-wrap justify-center gap-5">
           {Albums &&
             Albums.map((item, id) => (
               <Items
@@ -35,15 +40,14 @@ const GalleryItem = () => {
                 image={item.image}
               />
             ))}
-        </div>
+        </ul>
       </div>
-      <div>
-        <div className="py-4 text-center">
-          <h2>MOST POPULAR IN YOUR AREA</h2>
-        </div>
-        <div className="flex justify-center gap-5 overflow-auto">
-          {error && <p>{error}</p>}
 
+      <div className="bg-green-700 p-5">
+        <h2 className="text-center bg-red-400">MOST POPULAR IN YOUR AREA</h2>
+        {error && <p>{error}</p>}
+
+        <ul className="flex flex-wrap justify-center gap-5">
           {PopularItems &&
             PopularItems.map((item, id) => (
               <Items
@@ -53,8 +57,24 @@ const GalleryItem = () => {
                 image={item.image}
               />
             ))}
-        </div>
+        </ul>
       </div>
+
+      {/* <div className="bg-green-700 p-5">
+        <h2>MOST POPULAR IN YOUR AREA</h2>
+        {error && <p>{error}</p>}
+
+        <ul className="flex flex-wrap justify-center gap-5"></ul>
+        {PopularItems &&
+          PopularItems.map((item, id) => (
+            <Items
+              key={id}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+            />
+          ))}
+      </div> */}
     </div>
   );
 };
